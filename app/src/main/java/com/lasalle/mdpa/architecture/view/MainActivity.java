df64cbb248173e2375fef95c1c5e6234babdf451
albert.denova <albert.denova@king.com>
2018-02-01 20:37:50 +0100

    Add implementation

--- app/src/main/java/com/lasalle/mdpa/architecture/view/MainActivity.java
+++ app/src/main/java/com/lasalle/mdpa/architecture/view/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -19,6 +21,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.lasalle.mdpa.architecture.R;
+import com.lasalle.mdpa.architecture.model.TvShow;
 import com.lasalle.mdpa.architecture.view.adapter.TabAdapter;
 import com.lasalle.mdpa.architecture.view.model.LibraryViewModel;
 
@@ -39,15 +42,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
-        viewPager.setAdapter(new TabAdapter(getSupportFragmentManager()));
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_movie);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_tv_show);
-
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -56,6 +50,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        initFragmentManager();
     }
 
     @Override
@@ -90,15 +86,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
         switch(item.getItemId())
         {
-            case R.id.nav_first_fragment:
-                View rootView = findViewById(android.R.id.content);
-                Snackbar.make(rootView, R.string.snackbar_text, Snackbar.LENGTH_INDEFINITE).show();
+            case R.id.nav_movies:
+                fragment = MoviesFragment.newInstance();
+                break;
+
+            case R.id.nav_tv_show:
+                fragment = TvShowsFragment.newInstance();
                 break;
         }
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+        item.setChecked(true);
+        setTitle(item.getTitle());
+
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void initFragmentManager() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MoviesFragment fragment = MoviesFragment.newInstance();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
     }
 }
